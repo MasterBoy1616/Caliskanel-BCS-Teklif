8178a4b98791f80efcf91d6275397db0631c2bc8
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-18 14:59:44 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",  // Burası eklendi!
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000',
+    }
+  },
   root: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
 });
+
