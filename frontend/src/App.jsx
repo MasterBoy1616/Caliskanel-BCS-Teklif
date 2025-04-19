9b4a5be1ca1e16a7c9aa0c1e3ebfe3add5c42923
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-19 13:18:51 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import Admin from "./Admin";
 import Login from "./Login";
+import Admin from "./Admin";
 
 function App() {
   return (
     <Router>
-      <div className="app-background">
-        <nav className="bg-blue-600 text-white p-4 flex justify-center gap-8 mb-4">
-          <Link to="/">Anasayfa</Link>
-          <Link to="/login">Admin Giriş</Link>
-        </nav>
-
-        <div className="app-container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
     </Router>
   );
 }
