67610395af5a7d0a2d5f4a0f2408a66baa4e551f
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-21 10:49:56 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Home";
-import Admin from "./Admin";
 import Login from "./Login";
+import Admin from "./Admin";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/admin" element={isLoggedIn ? <Admin /> : <Navigate to="/login" />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
