dbe0856f7084cf7aa8e219aae886969f54d383fd
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-21 14:12:02 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
 import Admin from "./Admin";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
@@ -14,6 +13,6 @@ const App = () => {
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
