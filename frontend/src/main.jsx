1804501688c00f1031a213fa05154772c4dd5c0f
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-21 15:16:00 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./App.css";
+import axios from "axios";
+
+// Axios base ayarı (backend api'ye doğru istek atacak)
+axios.defaults.baseURL = "";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
