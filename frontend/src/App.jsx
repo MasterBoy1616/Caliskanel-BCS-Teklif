942746209d57da7248ac33a6b804c5ea14c53a07
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-21 17:21:46 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import Login from "./Login";
-import Admin from "./Admin";
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </Router>
+    <div className="app-background">
+      <Home />
+    </div>
   );
-};
+}
 
 export default App;
