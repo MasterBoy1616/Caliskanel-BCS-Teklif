da7f419156a74c27b58374a79a594be1fa09e5c9
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-21 17:44:35 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 
 function App() {
   return (
-    <div className="app-background">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
