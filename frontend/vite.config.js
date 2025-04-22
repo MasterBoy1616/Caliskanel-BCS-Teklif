6c0e75c402d8f137486899f4f8e41e4c3de50282
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-22 10:11:42 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
-    }
+      '/api': {
+        target: 'http://localhost:8000',  // Lokal çalışırken backend'e proxy olacak
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+    },
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      external: ['fsevents']  // MacOS bağımlılığı devre dışı bırakıldı
-    }
-  },
-  base: '/'
-});
+})
