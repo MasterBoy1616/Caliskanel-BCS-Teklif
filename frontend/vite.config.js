3990576b50f03d4c12e111b045591644a45bd946
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-22 15:27:24 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // ÖNEMLİ: tüm dosyaları ./assets/ şeklinde yüklemesi için
+  base: './',
   plugins: [react()],
   build: {
-    outDir: '../backend/dist', // Build çıktısı backend/dist klasörüne atılacak
-    emptyOutDir: true, // Build'den önce dist klasörünü temizler
+    outDir: '../backend/dist',
+    emptyOutDir: true,
   },
 });
