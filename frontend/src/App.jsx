b8ab7f502743c32d458ff26604aed34049fd5902
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-22 09:33:10 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+// frontend/src/App.jsx
+
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
