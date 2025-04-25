ea403f3c5a70b39a89dddac0009db7ca3926f11a
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-25 11:44:52 +0300

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,12 @@
-export default { root: "src" }
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../backend/dist',
+    emptyOutDir: true,
+  },
+  base: '/',
+})
