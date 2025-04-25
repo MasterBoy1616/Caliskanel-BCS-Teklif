8eff0bad13cf895bfb0c85480b787808c54d8648
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-25 12:12:20 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,2 +1,12 @@
-import React from "react";
-console.log("main dosyası")
+// frontend/src/main.jsx
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css" // varsa stilleri buradan alır
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
