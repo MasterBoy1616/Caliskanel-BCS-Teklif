bc1bdc1d80b2fc9fd2c576867e4a6cdb9dfbf9bb
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-25 13:58:01 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PriceCheck from "./PriceCheck";
+import PriceCheck from "./components/PriceCheck.jsx";
 
 const App = () => {
   return (
