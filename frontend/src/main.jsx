50bc15b8853205fd0c4d985c482d5213e57b2ecd
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-25 12:12:36 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,7 @@
-// frontend/src/main.jsx
-
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import "./index.css" // varsa stilleri buradan alır
+import "./index.css" // varsa
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
